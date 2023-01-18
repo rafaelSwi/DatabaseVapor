83aac39875cdff95b7ac48c1efb39e45d71d903c
Rafael Neuwirth Swierczynski <channelrns@gmail.com>
2023-01-17 22:25:49 -0300

    update

--- Tests/AppTests/Models+Testable.swift
+++ Tests/AppTests/Models+Testable.swift
@@ -12,3 +12,29 @@ extension User {
         return user
     }
 }
+
+extension Acronym {
+    static func create (
+        short: String = "TIL",
+        long: String = "Today I Learned",
+        user: User? = nil,
+        on database: Database
+    ) throws -> Acronym {
+        var acronymsUser = user
+        
+        if acronymsUser == nil {
+            acronymsUser = try User.create(on: database)
+        }
+        
+        let acronym = Acronym (
+            id: UUID(),
+            short: short,
+            long: long,
+            userID: acronymsUser!.id!
+        )
+        
+        try acronym.save(on: database).wait()
+        return acronym
+    }
+}
+
--- Tests/AppTests/UserTests.swift
+++ Tests/AppTests/UserTests.swift
@@ -19,27 +19,107 @@ final class UserTests: XCTestCase {
     }
     
     func testUsersCanRetrieveFromAPI() throws {
+        
+        let user = try User.create(
+            name: usersName,
+            username: usersUsername,
+            on: app.db
+        )
+        _ = try User.create(on: app.db)
+        
+        try app.test(.GET, usersURI, afterResponse: { response in
             
-            let user = try User.create(
-                name: usersName,
-                username: usersUsername,
-                on: app.db
-            )
-            _ = try User.create(on: app.db)
+            XCTAssertEqual(response.status, .ok)
             
-            try app.test(.GET, usersURI, afterResponse: { response in
+            let users = try response.content.decode([User].self)
+            
+            XCTAssertEqual(users.count, 2)
+            XCTAssertEqual(users[0].name, usersName)
+            XCTAssertEqual(users[0].username, usersUsername)
+            XCTAssertEqual(users[0].id, user.id)
+            
+        })
+    }
+    
+    func testUserCanBeSavedWithAPI () throws {
+        
+        let user = User(id: UUID(), name: usersName, username: usersUsername)
+        
+        try app.test(.POST, usersURI, beforeRequest: { request in
+            
+            try request.content.encode(user)
+            
+        }, afterResponse: { response in
+            
+            let receivedUser = try response.content.decode(User.self)
+            
+            XCTAssertEqual(receivedUser.name, usersName)
+            XCTAssertEqual(receivedUser.username, usersUsername)
+            XCTAssertNotNil(receivedUser.id)
+            
+            try app.test(.GET, usersURI, afterResponse: { secondResponse in
                 
-                XCTAssertEqual(response.status, .ok)
+                let decodedResponse = try secondResponse.content.decode ( [User].self )
                 
-                let users = try response.content.decode([User].self)
-                
-                XCTAssertEqual(users.count, 2)
-                XCTAssertEqual(users[0].name, usersName)
-                XCTAssertEqual(users[0].username, usersUsername)
-                XCTAssertEqual(users[0].id, user.id)
-                
+                XCTAssertEqual(decodedResponse.count, 1)
+                XCTAssertEqual(decodedResponse[0].name, usersName)
+                XCTAssertEqual(decodedResponse[0].username, usersUsername)
+                XCTAssertNotNil(decodedResponse[0].id)
             })
-        }
+        })
+    }
+    
+    func testGettingASingleUserFromTheAPI () throws {
+        
+        let user = try User.create (
+            name: usersName,
+            username: usersUsername,
+            on: app.db
+        )
+        
+        try app.test(.GET, "\(usersURI)/\(user.id!)", afterResponse: { response in
+            
+            let receivedUser = try response.content.decode(User.self)
+            
+            XCTAssertEqual(receivedUser.name, usersName)
+            XCTAssertEqual(receivedUser.username, usersUsername)
+            XCTAssertEqual(receivedUser.id, user.id)
+        })
+    }
+    
+    func testGettingAUsersAcronymsFromTheAPI() throws {
+        
+        let user = try User.create(on: app.db)
+        
+        let acronymShort = "OMG"
+        let acronymLong = "Oh My God"
+        
+        let acronym1 = try Acronym.create (
+            short: acronymShort,
+            long: acronymLong,
+            user: user,
+            on: app.db
+        )
+        
+        _ = try Acronym.create (
+            short: "LOL",
+            long: "Laugh Out Loud",
+            user: user,
+            on: app.db
+        )
+        
+        try app.test(.GET, "\(usersURI)\(user.id!)/acronyms", afterResponse: { response in
+            
+            let acronyms = try response.content.decode([Acronym].self)
+            
+            XCTAssertEqual(acronyms.count, 2)
+            XCTAssertEqual(acronyms[0].id, acronym1.id)
+            XCTAssertEqual(acronyms[0].short, acronym1.short)
+            XCTAssertEqual(acronyms[0].long, acronym1.long)
+        })
+    }
     
 }
+
+// 160
 
